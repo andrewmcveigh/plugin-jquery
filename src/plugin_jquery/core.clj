cb6d05b0ebfb810872704ece17e81aa5a9ef24fc
Andrew Mcveigh <me@andrewmcveigh.com>
2013-04-18 16:07:46 +0200

    Import

---
+++ src/plugin_jquery/core.clj
@@ -0,0 +1,7 @@
+(ns plugin-jquery.core)
+
+(def js
+  [:script {:src "/plugins/jquery/js/jquery.min.js"}])
+
+(def js-debug
+  [:script {:src "/plugins/jquery/js/jquery.js"}])
---
+++ test/plugin_jquery/core_test.clj
@@ -0,0 +1,7 @@
+(ns plugin-jquery.core-test
+  (:use clojure.test
+        plugin-jquery.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
